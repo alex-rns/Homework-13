09eefc8b7d32d3ba64c913f7ffbfa22da06c655a
alex-rns <alexrns3@gmail.com>
2017-12-27 00:13:26 +0200

    init

--- gulpfile.js
+++ gulpfile.js
@@ -23,9 +23,11 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'./node_modules/jquery/dist/jquery.min.js',
-        './node_modules/tether/dist/js/tether.min.js',
+        './node_modules/popper.js/dist/umd/popper.min.js',
+        // './node_modules/tether/dist/js/tether.min.js',
 		'./node_modules/bootstrap/dist/js/bootstrap.min.js',
 		'./node_modules/masonry-layout/dist/masonry.pkgd.min.js',
+
 		'src/js/common.min.js' // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
