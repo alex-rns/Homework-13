1b3be49ae4e70a8be1aec662c06a70f162a4c39a
alex-rns <alexrns3@gmail.com>
2017-12-27 17:29:48 +0200

    +welcome-slider

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,38 @@
 $(function() {
 
-	// Custom JS
+	$('#welcome-carousel').slick({
+        lazyLoad: 'ondemand',
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 544,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
+    })
 
 });
