8a38129ef741977bab0c13803cf4591ea1865227
alex-rns <alexrns3@gmail.com>
2017-12-28 23:43:21 +0200

    about us carousel responsive work

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,9 @@
 $(function() {
 
+    //CAROUSELS
+
+    //WELCOME-CAROUSEL
+
 	$('#welcome-carousel').slick({
         lazyLoad: 'ondemand',
         arrows: false,
@@ -35,11 +39,46 @@ $(function() {
 
     });
 
+    //ABOUT US CAROUSEL
+
 	$('#about-carousel').slick({
-        autoplay: true,
+        // autoplay: true,
         arrows: true,
         infinite: true,
-        slidesToShow: 5
+        slidesToShow: 5,
+        variableWidth: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
 	//Bootstrap dropdown on hover
