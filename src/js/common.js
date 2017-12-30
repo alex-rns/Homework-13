6310d5b6772013b6deffc79719c8daa92b9fe77b
alexrns <alexrns3@gmail.com>
2017-12-30 14:51:49 +0200

    project carousel

--- src/js/common.js
+++ src/js/common.js
@@ -80,6 +80,43 @@ $(function() {
         ]
     });
 
+    $('#project-carousel').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+        // responsive: [
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 544,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+
+    });
+
+
+
+
 	//Bootstrap dropdown on hover
 
     $('.dropdown').hover(function() {
